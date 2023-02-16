27343f4690ae71e8123f6fd1a3df96537e87fb27
AzrelixJ <84561314+AzrelixJ@users.noreply.github.com>
2023-02-16 13:47:46 +1100

    Update testinstall.sh

--- testinstall.sh
+++ testinstall.sh
@@ -1,7 +1,7 @@
 #azrelix ARM install script
 
 wget https://github.com/The-Yerbas-Endeavor/test/releases/download/test1/yerbas-arm64-.tar.gz
-tar -xvf yerbas-ubuntu20-.tar.gz
+tar -xvf yerbas-arm64-.tar.gz
 mkdir ~/.yerbascore && touch ~/.yerbascore/yerbas.conf
 echo daemon=1 >> ~/.yerbascore/yerbas.conf
 echo addnode=107.191.55.148 >> ~/.yerbascore/yerbas.conf
