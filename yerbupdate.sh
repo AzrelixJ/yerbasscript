49f0e683049dfa229e861c86ed51b1550ec5be19
AzrelixJ <84561314+AzrelixJ@users.noreply.github.com>
2023-04-03 10:03:19 +1000

    Update yerbupdate.sh

--- yerbupdate.sh
+++ yerbupdate.sh
@@ -3,7 +3,7 @@ cd yerbas-build/
 cd ../
 rm -r yerbas-build
 wget https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/yerbas-arm64-v3.1.2.6.tar.gz
-tar -xf yerbas-arm64-v3.1.1.5.tar.gz
-rm yerbas-arm64-v3.1.1.5.tar.gz
+tar -xf yerbas-arm64-v3.1.1.6.tar.gz
+rm yerbas-arm64-v3.1.1.6.tar.gz
 cd yerbas-build
 ./yerbasd
