164e360d9d22e93bfd558cea3177d2eb25f5b330
AzrelixJ <84561314+AzrelixJ@users.noreply.github.com>
2023-04-03 10:07:23 +1000

    Delete yerbupdate1.sh

--- yerbupdate1.sh
+++
@@ -1,9 +0,0 @@
-cd yerbas-build/
-./yerbas-cli stop
-cd ../
-rm -r yerbas-build
-wget https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/yerbas-arm64-v3.1.2.6.tar.gz
-tar -xf yerbas-arm64-v3.1.1.6.tar.gz
-rm yerbas-arm64-v3.1.1.6.tar.gz
-cd yerbas-build
-./yerbasd
