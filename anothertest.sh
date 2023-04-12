2da9f494bdb8e405a61958c350fa0ae0bb4182f7
AzrelixJ <84561314+AzrelixJ@users.noreply.github.com>
2023-04-12 12:42:59 +1000

    update

--- anothertest.sh
+++ anothertest.sh
@@ -1,2 +1,30 @@
-echo "Test"
-echo "Yay this worked"
+#azrelix ARM install script
+
+
+echo "Azrelix Yerbas ARM install Script"
+echo " "
+read -p "Enter username: " uname
+read -p "Enter IP address: " ipaddy
+read -p "Enter BLS private key: " blsprivkey
+
+echo $uname
+echo $ipaddy
+echo $blsprivkey
+
+sudo useradd -m "$uname" -p "test"
+
+echo "rpcport=100"$uname
+
+
+wget https://github.com/The-Yerbas-Endeavor/test/releases/download/test1/yerbas-arm64-.tar.gz
+tar -xvf yerbas-arm64-.tar.gz
+rm yerbas-arm64-.tar.gz
+mkdir ~/.yerbascore && touch ~/.yerbascore/yerbas.conf
+echo daemon=1 >> ~/.yerbascore/yerbas.conf
+echo addnode=107.191.55.148 >> ~/.yerbascore/yerbas.conf
+echo smartnodeblsprivkey=$blsprivkey >> ~/.yerbascore/yerbas.conf
+echo rpcport= >> ~/.yerbascore/yerbas.conf
+echo bind=$ipaddy >> ~/.yerbascore/yerbas.conf
+cd ~/yerbas-build/
+cat <(crontab -l) <(echo "@reboot sleep 20 && ~/yerbas-build/yerbasd -testnet") | crontab -
+nano ~/.yerbascore/yerbas.conf 
--- updatescript.sh
+++ updatescript.sh
@@ -1,27 +1,41 @@
+
 osType="$(uname -m)"
+COIN_DAEMON='yerbasd'
+COIN_NAME='YERBAS'
+COIN_CLI='yerbas-cli'
+COIN_URL='https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/'
+COIN_URL_ARM='yerbas-arm64-v3.1.2.6.tar.gz'
+COIN_URL_UBUNTU='yerbas-ubuntu20-v3.1.2.6.tar.gz'
+COIN_URL_POWER='powcache.dat'
+COIN_URL_BOOT='bootstrap.zip'
+COIN_VERSION_NAME='v3.1.2.6 -- Salata Furca Deux'
+NODE_IP=""
+YERBAS_GREEN="\033[0;32m"
+
+
 clear
 
-echo "YYYYYYY       YYYYYYYEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   BBBBBBBBBBBBBBBBB               AAA                 SSSSSSSSSSSSSSS   "
-echo "Y:::::Y       Y:::::YE::::::::::::::::::::ER::::::::::::::::R  B::::::::::::::::B             A:::A              SS:::::::::::::::S  "
-echo "Y:::::Y       Y:::::YE::::::::::::::::::::ER::::::RRRRRR:::::R B::::::BBBBBB:::::B           A:::::A            S:::::SSSSSS::::::S  "
-echo "Y::::::Y     Y::::::YEE::::::EEEEEEEEE::::ERR:::::R     R:::::RBB:::::B     B:::::B         A:::::::A           S:::::S     SSSSSSS  "
-echo "YYY:::::Y   Y:::::YYY  E:::::E       EEEEEE  R::::R     R:::::R  B::::B     B:::::B        A:::::::::A          S:::::S              "
-echo "   Y:::::Y Y:::::Y     E:::::E               R::::R     R:::::R  B::::B     B:::::B       A:::::A:::::A         S:::::S              "
-echo "    Y:::::Y:::::Y      E::::::EEEEEEEEEE     R::::RRRRRR:::::R   B::::BBBBBB:::::B       A:::::A A:::::A         S::::SSSS           "
-echo "     Y:::::::::Y       E:::::::::::::::E     R:::::::::::::RR    B:::::::::::::BB       A:::::A   A:::::A         SS::::::SSSSS      "
-echo "      Y:::::::Y        E:::::::::::::::E     R::::RRRRRR:::::R   B::::BBBBBB:::::B     A:::::A     A:::::A          SSS::::::::SS    "
-echo "       Y:::::Y         E::::::EEEEEEEEEE     R::::R     R:::::R  B::::B     B:::::B   A:::::AAAAAAAAA:::::A            SSSSSS::::S   "
-echo "       Y:::::Y         E:::::E               R::::R     R:::::R  B::::B     B:::::B  A:::::::::::::::::::::A                S:::::S  "
-echo "       Y:::::Y         E:::::E       EEEEEE  R::::R     R:::::R  B::::B     B:::::B A:::::AAAAAAAAAAAAA:::::A               S:::::S  "
-echo "       Y:::::Y       EE::::::EEEEEEEE:::::ERR:::::R     R:::::RBB:::::BBBBBB::::::BA:::::A             A:::::A  SSSSSSS     S:::::S  "
-echo "    YYYY:::::YYYY    E::::::::::::::::::::ER::::::R     R:::::RB:::::::::::::::::BA:::::A               A:::::A S::::::SSSSSS:::::S  "
-echo "    Y:::::::::::Y    E::::::::::::::::::::ER::::::R     R:::::RB::::::::::::::::BA:::::A                 A:::::AS:::::::::::::::SS   "
-echo "    YYYYYYYYYYYYY    EEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRRBBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAASSSSSSSSSSSSSSS     "
+echo "${GREEN}YYYYYYY       YYYYYYYEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   BBBBBBBBBBBBBBBBB               AAA                 SSSSSSSSSSSSSSS   "
+echo "${GREEN}Y:::::Y       Y:::::YE::::::::::::::::::::ER::::::::::::::::R  B::::::::::::::::B             A:::A              SS:::::::::::::::S  "
+echo "${GREEN}Y:::::Y       Y:::::YE::::::::::::::::::::ER::::::RRRRRR:::::R B::::::BBBBBB:::::B           A:::::A            S:::::SSSSSS::::::S  "
+echo "${GREEN}Y::::::Y     Y::::::YEE::::::EEEEEEEEE::::ERR:::::R     R:::::RBB:::::B     B:::::B         A:::::::A           S:::::S     SSSSSSS  "
+echo "${GREEN}YYY:::::Y   Y:::::YYY  E:::::E       EEEEEE  R::::R     R:::::R  B::::B     B:::::B        A:::::::::A          S:::::S              "
+echo "${GREEN}   Y:::::Y Y:::::Y     E:::::E               R::::R     R:::::R  B::::B     B:::::B       A:::::A:::::A         S:::::S              "
+echo "${GREEN}    Y:::::Y:::::Y      E::::::EEEEEEEEEE     R::::RRRRRR:::::R   B::::BBBBBB:::::B       A:::::A A:::::A         S::::SSSS           "
+echo "${GREEN}     Y:::::::::Y       E:::::::::::::::E     R:::::::::::::RR    B:::::::::::::BB       A:::::A   A:::::A         SS::::::SSSSS      "
+echo "${GREEN}      Y:::::::Y        E:::::::::::::::E     R::::RRRRRR:::::R   B::::BBBBBB:::::B     A:::::A     A:::::A          SSS::::::::SS    "
+echo "${GREEN}       Y:::::Y         E::::::EEEEEEEEEE     R::::R     R:::::R  B::::B     B:::::B   A:::::AAAAAAAAA:::::A            SSSSSS::::S   "
+echo "${GREEN}       Y:::::Y         E:::::E               R::::R     R:::::R  B::::B     B:::::B  A:::::::::::::::::::::A                S:::::S  "
+echo "${GREEN}       Y:::::Y         E:::::E       EEEEEE  R::::R     R:::::R  B::::B     B:::::B A:::::AAAAAAAAAAAAA:::::A               S:::::S  "
+echo "${GREEN}       Y:::::Y       EE::::::EEEEEEEE:::::ERR:::::R     R:::::RBB:::::BBBBBB::::::BA:::::A             A:::::A  SSSSSSS     S:::::S  "
+echo "${GREEN}    YYYY:::::YYYY    E::::::::::::::::::::ER::::::R     R:::::RB:::::::::::::::::BA:::::A               A:::::A S::::::SSSSSS:::::S  "
+echo "${GREEN}    Y:::::::::::Y    E::::::::::::::::::::ER::::::R     R:::::RB::::::::::::::::BA:::::A                 A:::::AS:::::::::::::::SS   "
+echo "${GREEN}    YYYYYYYYYYYYY    EEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRRBBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAASSSSSSSSSSSSSSS     "
 
 
 echo ""
 echo ""
-echo "Azrelix YERBAS update script ...."
+echo "YERBAS install script .... created and maintained by Azrelix"
 echo ""
 read -t 0.5 -p "Detecting system "
 read -t 0.5 -p "."
@@ -50,7 +64,7 @@ read -t 0.5 -p "."
 read -t 0.5 -p "."
 read -t 0.1 -p ". "
 cd ~/yerbas-build
-./yerbas-cli stop
+./$COIN_CLI stop
 cd ~
 read -t 1 -p "Success."
 echo " "
@@ -64,17 +78,17 @@ rm -r yerbas-build
 read -t 1 -p "Success."
 echo " "
 
-read -t 0.5 -p "Fetching yerbas v3.2.16 Salata Furca Deux "
+read -t 0.5 -p "Fetching " $COIN_VERSION_NAME
 read -t 0.5 -p "."
 read -t 0.5 -p "."
 read -t 0.5 -p "."
 read -t 0.1 -p ". "
 if [ $osType == "x86_64" ]
         then
-        wget https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/yerbas-ubuntu20-v3.1.2.6.tar.gz
+        wget -q $COIN_URL$COIN_URL_UBUNTU
 elif [ $osType == "aarch64" ]
         then
-        wget https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/yerbas-arm64-v3.1.2.6.tar.gz
+        wget -q $COIN_URL$COIN_URL_ARM
 fi
 read -t 1 -p "Success."
 echo " "
@@ -86,23 +100,25 @@ read -t 0.5 -p "."
 read -t 0.1 -p ". "
 if [ $osType == "x86_64" ]
         then
-        tar -xf yerbas-ubuntu20-v3.1.2.6.tar.gz
-        rm yerbas-ubuntu20-v3.1.2.6.tar.gz
+        tar -xf $COIN_URL_UBUNTU
+        rm $COIN_URL_UBUNTU
 elif [ $osType == "aarch64" ]
         then
-        tar -xf yerbas-arm64-v3.1.2.6.tar.gz
-        rm yerbas-arm64-v3.1.2.6.tar.gz
+        tar -xf $COIN_URL_ARM
+        rm $COIN_URL_ARM
 fi
 read -t 1 -p "Success."
 echo " "
 
+sudo apt-get install -qq unzip
+
 cd ~/.yerbascore
-wget -q https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/powcache.dat
-wget -q https://github.com/The-Yerbas-Endeavor/yerbas/releases/download/v3.1.2.6/bootstrap.zip
-unzip bootstrap.zip
+wget -q $COIN_URL$COIN_URL_POWER
+wget -q $COIN_URL$COIN_URL_BOOT
+unzip $COIN_URL_BOOT
 mv bootstrap/*
 rm -r bootstrap
-rm bootstrap.zip
+rm $COIN_URL_BOOT
 
 read -t 0.5 -p "Restarting Yerbas Daemon "
 read -t 0.5 -p "."
@@ -110,12 +126,12 @@ read -t 0.5 -p "."
 read -t 0.5 -p "."
 read -t 0.1 -p ". "
 cd ~/yerbas-build
-./yerbasd
+./COIN_DAEMON
 cd ~
 read -t 1 -p "Success."
 echo " "
 echo " "
 echo " "
 echo " "
-read -t 2 -p "UPDATE COMPLETE 420 4LIFE"
+read -t 2 -p "UPDATE COMPLETE ... 420 4LIFE"
 echo " "
