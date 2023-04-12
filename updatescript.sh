7293cefd4a24c62247c9801993815bd417eb7258
AzrelixJ <84561314+AzrelixJ@users.noreply.github.com>
2023-04-12 19:58:35 +1000

    update2

--- updatescript.sh
+++ updatescript.sh
@@ -11,6 +11,7 @@ COIN_URL_BOOT='bootstrap.zip'
 COIN_VERSION_NAME='v3.1.2.6 -- Salata Furca Deux'
 NODE_IP=''
 INS_TYPE=''
+File=".err.log"
 
 YG='\033[0;32m'
 CN='\033[0m'
@@ -39,7 +40,8 @@ function yerbas_title() {
 
         echo ""
         echo ""
-        echo "YERBAS install script .... created and maintained by Azrelix"
+        echo "$COIN_NAME install script .... created and maintained by Azrelix"
+        echo ""
         echo ""
 
 }
@@ -56,25 +58,19 @@ function install_type(){
         echo "Select option"
         echo "  1) Install new $COIN_NAME node"
         echo "  2) Update existing $COIN_NAME node"
-
-
         read n
         case $n in
         1) INS_TYPE='new';;
         2) INS_TYPE='update';;
-        *) echo "invalid option selected.. :( try again"  ;install_type;
+        *) echo -e "${RED}invalid option selected.. :( try again${CN}"  ;install_type;
         esac
-
-
+        echo " "
 }
 
 
 function therest() {
         read -t 0.5 -p "Detecting system "
         dots
-
-
-
         if [ $osType == "x86_64" ]
                 then
                 echo -e "${YG}$osType system detected.${CN}"
@@ -90,28 +86,38 @@ function therest() {
 }
 
 function uninstall_old() {
-
-        read -t 0.5 -p "Stopping Yerbas Damon "
-        dots
-
-        cd ~/yerbas-build
-        ./$COIN_CLI stop
-        cd ~
-        read -t 1 -p " "
-        echo -e "${YG}Success.${CN}"
-        echo " "
-
-        read -t 0.5 -p "Removing YerbasBuild folder "
-        dots
-
-        rm -r yerbas-build
-        read -t 1 -p "Success."
-        echo " "
-
+        if [ $INS_TYPE == "update" ]
+                then
+                read -t 0.5 -p "Stopping Yerbas Damon "
+                dots
+                cd ~/yerbas-build &> ~/.err.log
+                if ! grep -q "No such file or directory" "$File"; then
+                        ./$COIN_CLI stop &> ~/.err.log
+                        cd ~
+                        if ! grep -q "error:" "$File"; then
+                                echo -e "${YG}Success.${CN}"
+                        else
+                                echo -e "${RED}Failed.  Daemon not running, attempting to continue.${CN}"
+                        fi
+                        echo " "
+                        read -t 0.5 -p "Removing YerbasBuild folder "
+                        dots
+                        rm -r yerbas-build &> ~/.err.log
+                        if ! grep -q "cannot remove 'yerbas-build'" "$File"; then
+                                echo -e "${YG}Success.${CN}"
+                        else
+                                echo -e "${RED}Failed. Unable to remove yerbas-build folder, either wrong location or yerbas not installed, attempting to continue.${CN}"
+                        fi
+                else
+                        echo -e "${RED}yerbas-build directory not found, either wrong location or yerbas not installed, attempting to continue.${CN}"
+                fi
+                
+                echo " "
+        fi
 }
 
 function download_node() {
-        read -t 0.5 -p "Fetching  $COIN_VERSION_NAME"
+        read -t 0.5 -p "Fetching  $COIN_NAME $COIN_VERSION_NAME"
         dots
 
         if [ $osType == "x86_64" ]
@@ -121,12 +127,10 @@ function download_node() {
                 then
                 wget -q $COIN_URL$COIN_URL_ARM
         fi
-        read -t 1 -p "Success."
+        echo -e "${YG}Success.${CN}"
         echo " "
-
         read -t 0.5 -p "Extracting files "
         dots
-
         if [ $osType == "x86_64" ]
                 then
                 tar -xf $COIN_URL_UBUNTU
@@ -136,15 +140,22 @@ function download_node() {
                 tar -xf $COIN_URL_ARM
                 rm $COIN_URL_ARM
         fi
-        read -t 1 -p "Success."
+        echo -e "${YG}Success.${CN}"
         echo " "
 
 }
 
+function power_cache() {
+
+
+
+        
+}
+
+
+
 function other() {
-
         sudo apt-get install -qq unzip
-
         cd ~/.yerbascore
         wget -q $COIN_URL$COIN_URL_POWER
         wget -q $COIN_URL$COIN_URL_BOOT
@@ -153,20 +164,16 @@ function other() {
         rm -r bootstrap
         rm $COIN_URL_BOOT
         }
-
         function restart_daemon() {
         read -t 0.5 -p "Restarting Yerbas Daemon "
         dots
-
         cd ~/yerbas-build
         ./COIN_DAEMON
         cd ~
-        read -t 1 -p "Success."
+        echo -e "${YG}Success.${CN}"
         dots
-
-        read -t 2 -p "UPDATE COMPLETE ... 420 4LIFE"
+        read -t 2 -p "${YG}UPDATE COMPLETE ... 420 4 LIFE${CN}"
         echo " "
-
 }
 
 #MAIN
